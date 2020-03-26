349b6dde6fe2dfc52a221735e528695c5a71d2af
orestis <orestispanago@gmail.com>
2020-03-26 18:26:22 +0200

    Process done manually in QGIS using SQL queries

---
+++ groupping.sql,.sql
@@ -0,0 +1,19 @@
+SELECT Alias,area,geometry,
+CASE 
+WHEN LOWER(ITEM2012) LIKE "%continuous%" THEN "urban"
+WHEN LOWER(ITEM2012) LIKE "%green%" 
+OR LOWER(ITEM2012) LIKE "%pastures%"
+OR LOWER(ITEM2012) LIKE "%crops%"
+THEN "green"
+WHEN LOWER(ITEM2012) LIKE "%roads%" 
+THEN "roads"
+WHEN LOWER(ITEM2012) LIKE "%industrial%"
+OR LOWER(ITEM2012) LIKE "%structures%"
+OR LOWER(ITEM2012) LIKE "%without%"
+OR LOWER(ITEM2012) LIKE "%mineral%"
+OR LOWER(ITEM2012) LIKE "%rail%"
+OR LOWER(ITEM2012) LIKE "%sports%"
+OR LOWER(ITEM2012) LIKE "%water%"
+THEN "other"
+END LCTYPE /*:text*/
+FROM added_geom_info
---
+++ joining_all.sql,.sql
@@ -0,0 +1,3 @@
+SELECT buffered.geometry,buffered.Alias,QueryLayer.LCTYPE,QueryLayer.LCRATIO
+FROM (buffered
+INNER JOIN QueryLayer ON buffered.Alias=QueryLayer.Alias)
